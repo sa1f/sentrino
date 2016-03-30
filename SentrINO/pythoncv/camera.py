bdccb304b2d8630ea2e2e5015c0d9306bf604431
ValRat <leri.ratu@gmail.com>
2016-03-29 18:39:04 -0700

    Motion + Color Detection

--- SentrINO/pythoncv/camera.py
+++ SentrINO/pythoncv/camera.py
@@ -1,6 +1,7 @@
 import cv2
 
 class VideoCamera(object):
+
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
@@ -9,15 +10,50 @@ class VideoCamera(object):
         # If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
         self.video = cv2.VideoCapture(0)
-    
+        self.minArea = 1000 
+
     def __del__(self):
         self.video.release()
     
     def get_frame(self):
-        success, image = self.video.read()
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
         # video stream.
-        image = cv2.resize(image, None, fx=0.2, fy=0.2)
+        differenceSet = False
+        firstFrame = None
+        frame = None
+        while not differenceSet:
+            (grabbed, frame) = self.video.read()
+            if not grabbed:
+                break
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            if firstFrame is None:
+                firstFrame = gray
+                continue
+
+            differenceSet = True
+            frameDelta = cv2.absdiff(firstFrame, gray)
+            thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.dilate(thresh, None, iterations = 2)
+            (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            firstFrame = None
+            maxArea = 0
+            maxContour = None
+            for c in cnts:
+                currArea = cv2.contourArea(c)
+                if currArea > self.minArea:
+                    if currArea > maxArea:
+                        maxArea = currArea
+                        maxContour = c
+                        print currArea
+    
+            if maxContour != None:
+                (x, y, w, h) = cv2.boundingRect(maxContour)
+                cv2.rectangle(frame, (x, y), (x + w, y +  h), (0, 255, 0), 2)
+
+                
+        image = cv2.resize(frame, None, fx=0.2, fy=0.2)
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tostring()
--- SentrINO/pythoncv/motiondetection.py
+++ SentrINO/pythoncv/motiondetection.py
@@ -1,41 +1,49 @@
 import cv2
 
 camera = cv2.VideoCapture(0)
-minArea = 10
+minArea = 5000
 firstFrame = None 
 
-while True:
-    (grabbed, frame) = camera.read()
-    if not grabbed:
-        break
+try:
+    while True:
+        (grabbed, frame) = camera.read()
+        if not grabbed:
+            break
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    if firstFrame is None:
-        firstFrame = gray
-        continue
-    
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.dilate(thresh, None, iterations = 2)
-    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    firstFrame = None
-    for c in cnts:
-        if cv2.contourArea(c) < minArea:
+        if firstFrame is None:
+            firstFrame = gray
             continue
+        
+        frameDelta = cv2.absdiff(firstFrame, gray)
+        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations = 2)
+        (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        firstFrame = None
+        maxArea = 0
+        maxContour = None
+        for c in cnts:
+            currArea = cv2.contourArea(c)
+            if currArea > minArea:
+                if currArea > maxArea:
+                    maxArea = currArea
+                    maxContour = c
+                    print currArea
+        
+        if maxContour != None:
+            (x, y, w, h) = cv2.boundingRect(maxContour)
+            cv2.rectangle(frame, (x, y), (x + w, y +  h), (0, 255, 0), 2)
 
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y +  h), (0, 255, 0), 2)
+        cv2.imshow("Input", frame)
+        cv2.imshow("Threshold", thresh)
+        cv2.imshow("Frame Delta", frameDelta)
+        key = cv2.waitKey(1) & 0xFF
 
-    cv2.imshow("Input", frame)
-    cv2.imshow("Threshold", thresh)
-    cv2.imshow("Frame Delta", frameDelta)
-    key = cv2.waitKey(1) & 0xFF
-
-    if key == ord("q"):
-        break
-
-camera.release()
-cv2.destroyAllWindows()
+        if key == ord("q"):
+            break
+finally:
+    camera.release()
+    cv2.destroyAllWindows()
 
