e9a01a31053c676363db07b769ecff4b72468636
Saif Sajid <saifmsajid@gmail.com>
2016-04-03 02:16:44 -0700

    added manual/auto mode switching

--- website/listener.py
+++ website/listener.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
-#
-# Project: Video Streaming with Flask
-# Author: Log0 <im [dot] ckieric [at] gmail [dot] com>
-# Date: 2014/12/21
-# Website: http://www.chioka.in/
-# Description:
-# Modified to support streaming out with webcams, and not just raw JPEGs.
-# Most of the code credits to Miguel Grinberg, except that I made a small tweak. Thanks!
-# Credits: http://blog.miguelgrinberg.com/post/video-streaming-with-flask
-#
-# Usage:
-# 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
-# 2. Run "python main.py".
-# 3. Navigate the browser to the local webpage.
 from flask import Flask, render_template, Response
 import serial
+import time
+import threading
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 app = Flask(__name__)
 
+auto = True
+def manualMode():
+    global auto
+    auto = False
+    time.sleep(30)
+    auto = True
 
 @app.route('/move/<direction>')
 def move(direction):
+
+    # Non blocking method for switching
+    # to manual mode and switching back to auto mode
+    # after 30 seconds
+    t = threading.Thread(target=manualMode)
+    t.start()
+
     if direction == 'up':
         ser.write('u')
     elif direction == 'down':
@@ -33,6 +34,18 @@ def move(direction):
         ser.write('l')
     return direction
 
+@app.route('/automove/<direction>')
+def move(direction):
+    if auto:
+        if direction == 'up':
+            ser.write('u')
+        elif direction == 'down':
+            ser.write('d')
+        elif direction == 'right':
+            ser.write('r')
+        elif direction == 'left':
+            ser.write('l')
+        return direction
 
 if __name__ == '__main__':
     app.debug = True
