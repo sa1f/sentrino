360da318bee2a04a4d428f66e4587228f0ecf2cf
Saif Sajid <saifmsajid@gmail.com>
2016-04-03 02:46:49 -0700

    consolidated sms functionality into website script

--- website/main.py
+++ website/main.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, Response
 from camera import VideoCamera
+import requests
+import json
+import os.path
 
 app = Flask(__name__)
+
+if not os.path.isfile('info.json'):
+    print "Please give information. This will be saved in info.json and used for future requests"
+    username = raw_input("Enter your username: ")
+    password = raw_input("Enter your passowrd: ")
+    did = raw_input("Enter your did number: ")
+    dst = raw_input("Enter your destination number: ")
+    message = raw_input("Enter your message: ")
+    infoIn = {"api_username": username, "api_password": password, "did": did, "dst": dst, "message": message,
+            'method' : 'sendSMS'}
+    with open('info.json', 'w') as outfile:
+        json.dump(infoIn, outfile)
+
+with open('info.json', 'r') as infile:
+    global info
+    info = json.load(infile)
 
 @app.route('/')
 def index():
@@ -19,7 +38,10 @@ def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/alert/<msg>')
+def alert(msg):
+    info['message'] = msg
+    print requests.get('https://voip.ms/api/v1/rest.php', params=info).text
+
 if __name__ == '__main__':
-    app.debug = True
-    #app.run()
     app.run(host='0.0.0.0', debug=True)
