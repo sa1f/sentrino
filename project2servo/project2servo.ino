5f87b24960d1a3ef0cf1d04674cbb1cadd2cd310
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-04-04 15:35:06 -0700

    Music test

--- project2servo/project2servo.ino
+++ project2servo/project2servo.ino
@@ -1,16 +1,20 @@
+#include <LiquidCrystal.h>
 #include <Servo.h>
 
-Servo yaw;    //For controlling left-right movement
-Servo pitch;  //For controlling up-down movement
+//LiquidCrystal screen(8, 9, 10, 11, 12, 13); //RS, EN, D4, D5, D6, D7
 
-//PIN CONSTANTS
-const int YAW_PIN = 9;
-const int PITCH_PIN = 10;
+Servo yaw;    //ASSIGNED PIN 9 : For controlling left-right movement
+Servo pitch;  //ASSIGNED PIN 10 : For controlling up-down movement
 
 //VALUE CONSTANTS
-const int CENTER = 90;
-const int RIGHT = 0;
-const int LEFT = 180;
+const int CENTER_MOST = 90;
+const int RIGHT_MOST = 0;
+const int LEFT_MOST = 180;
+
+//LCD CONSTANTS
+const int FIRST_COL = 0;
+const int TOP_ROW = 1;
+const int BOTTOM_ROW = 1;
 
 /**
  * Rotate a given servo arm relative to its current position
@@ -23,9 +27,9 @@ void turnServo(Servo servo, bool clockwise, int motorStep=1){
   int pos = servo.read();
   
   if(clockwise){
-    servo.write((pos - motorStep >= 0) ? pos - motorStep : 0);  
+    servo.write((pos - motorStep >= RIGHT_MOST) ? pos - motorStep : 0);  
   }else{
-    servo.write((pos + motorStep <= 180) ? pos + motorStep : 0);  
+    servo.write((pos + motorStep <= LEFT_MOST) ? pos + motorStep : 0);  
   }
 }
 
@@ -33,23 +37,27 @@ void turnServo(Servo servo, bool clockwise, int motorStep=1){
 /**
  * FOR DEBUGGING PURPOSE ONLY, DELETE THIS BEFORE SUBMITTING CODE
  */
-void testLoop(){
-  Serial.println("LEFT");
+/*void testLoop(){
+  screen.clear();
+  screen.print("TURNING LEFT");
   turnServo(yaw,true, 30);
   delay(1000);
-
-  Serial.println("RIGHT");
+  
+  screen.clear();
+  screen.print("TURNING RIGHT");
   turnServo(yaw,false, 30);
   delay(1000);
 
-  Serial.println("UP");
+  screen.clear();
+  screen.print("TURNING UP");
   turnServo(pitch,true, 30);
   delay(1000);
 
-  Serial.println("DOWN");
+  screen.clear();
+  screen.print("TURNING DOWN");
   turnServo(pitch,false, 30);
   delay(1000);
-}
+}*/
 
 
 /**
@@ -58,11 +66,14 @@ void testLoop(){
 void setup() {
   Serial.begin(9600);
   
+  //screen.begin(16, 2);
+  audio_setup();
+  
   yaw.attach(9);
   pitch.attach(10);
   
-  yaw.write(90);
-  pitch.write(90);
+  yaw.write(CENTER_MOST);
+  pitch.write(CENTER_MOST);
 }
 
 
@@ -70,12 +81,13 @@ void setup() {
  * The main loop of our program
  */
 void loop() {
-  if (Serial.available()){ 
+  /*if (Serial.available()){ 
     switch (Serial.read()) {
       case 'l': turnServo(yaw, true, 3); break;
       case 'r': turnServo(yaw, false, 3); break;
       case 'u': turnServo(pitch, true, 3); break;
       case 'd': turnServo(pitch, false, 3); break;
     }
-  }
+  }8*/
+  audio_loop();
 }
