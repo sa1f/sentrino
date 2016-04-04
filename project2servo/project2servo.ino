88f9ee18becf26dfd406d091c420ada1504e008c
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-04-04 14:56:57 -0700

    Greatly Refactored and commented servo code

--- project2servo/project2servo.ino
+++ project2servo/project2servo.ino
@@ -1,83 +1,81 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
 
-Servo myservolr;  // create servo object to control a servo
-Servo myservoud;
-// twelve servo objects can be created on most boards
+Servo yaw;    //For controlling left-right movement
+Servo pitch;  //For controlling up-down movement
 
-int poslr;
-int posud;
-int action;
-int next;
+//PIN CONSTANTS
+const int YAW_PIN = 9;
+const int PITCH_PIN = 10;
 
-void setup() {
-  Serial.begin(9600);
-  myservolr.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservoud.attach(10);
-  myservolr.write(90);
-  myservoud.write(90);
- 
-}
+//VALUE CONSTANTS
+const int CENTER = 90;
+const int RIGHT = 0;
+const int LEFT = 180;
 
-void loop() {
-  poslr = myservolr.read();
-  posud = myservoud.read();
-  if (Serial.available()){ 
-    action = Serial.read();
-    /*
-    next = Serial.peek();
-    if (isDigit(next)) { 
-      next = Serial.read();
-    }  
-    else {
-      next = 1;
-    }
-    */
-    switch (action) {
-      case 'l': rotateLeft(poslr); break;
-      case 'r': rotateRight(poslr); break;
-      case 'u': panUp(posud); break;
-      case 'd': panDown(posud); break;
-       
-    }
+/**
+ * Rotate a given servo arm relative to its current position
+ * 
+ * @param servo - The servo to rotate
+ * @param clockwise - Determines whether to rotate clockwise or not
+ * @param motorStep - How much to degree to rotate
+ */
+void turnServo(Servo servo, bool clockwise, int motorStep=1){
+  int pos = servo.read();
+  
+  if(clockwise){
+    servo.write((pos - motorStep >= 0) ? pos - motorStep : 0);  
+  }else{
+    servo.write((pos + motorStep <= 180) ? pos + motorStep : 0);  
   }
 }
 
-void rotateLeft(int poslr) {
-  if (poslr+3 <= 180) 
-    myservolr.write((poslr+3));
-  else 
-    myservolr.write(180);
-}
 
-void rotateRight(int poslr) {
-  if (poslr-3 >= 0) 
-    myservolr.write((poslr-3));
-  else
-    myservolr.write(0);
-}
+/**
+ * FOR DEBUGGING PURPOSE ONLY, DELETE THIS BEFORE SUBMITTING CODE
+ */
+void testLoop(){
+  Serial.println("LEFT");
+  turnServo(yaw,true, 30);
+  delay(1000);
 
+  Serial.println("RIGHT");
+  turnServo(yaw,false, 30);
+  delay(1000);
 
-void panUp(int posud) {
-  if (posud-3 >= 0)
-    myservoud.write((posud-3));
-  else
-    myservoud.write(0);
-}
+  Serial.println("UP");
+  turnServo(pitch,true, 30);
+  delay(1000);
 
-void panDown(int posud) {
-  if (posud+3 <= 180)
-    myservoud.write((posud+3));
-  else
-    myservoud.write(180);
+  Serial.println("DOWN");
+  turnServo(pitch,false, 30);
+  delay(1000);
 }
 
 
+/**
+ * Set-ups required before the main loop
+ */
+void setup() {
+  Serial.begin(9600);
+  
+  yaw.attach(9);
+  pitch.attach(10);
+  
+  yaw.write(90);
+  pitch.write(90);
+}
+
+
+/**
+ * The main loop of our program
+ */
+void loop() {
+  if (Serial.available()){ 
+    switch (Serial.read()) {
+      case 'l': turnServo(yaw, true, 3); break;
+      case 'r': turnServo(yaw, false, 3); break;
+      case 'u': turnServo(pitch, true, 3); break;
+      case 'd': turnServo(pitch, false, 3); break;
+    }
+  }
+}
