2e5f36fff8d8f55aaf72158a7569d395c82c611b
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-04-04 23:08:02 -0700

    Alarm code working

--- project2servo/project2servo.ino
+++ project2servo/project2servo.ino
@@ -7,12 +7,15 @@ LiquidCrystal screen(4, 5, 12, 13, 7, 8); //RS, EN, D4, D5, D6, D7
 Servo yaw;    //ASSIGNED PIN 9 : For controlling left-right movement
 Servo pitch;  //ASSIGNED PIN 10 : For controlling up-down movement
 
+const int ENEMY_LED = 6;
+const int PIEZO_PIN = 11;
+
 //VALUE CONSTANTS
 const int CENTER_MOST = 90;
 const int RIGHT_MOST = 0;
 const int LEFT_MOST = 180;
 
-
+//FOR KEYPAD ALARM DISABLING
 const int KEY_BUFFER_SIZE = 16;
 char keyBuffer[KEY_BUFFER_SIZE];
 int keyBufferHead = 0;
@@ -27,6 +30,8 @@ char keys[ROWS][COLS] = {
 };
 byte rowPins[ROWS] = {3, 14, 15, 16}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {17, 18, 19}; //connect to the column pinouts of the keypad
+
+bool alarmIsOn = false;
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
@@ -47,33 +52,9 @@ void turnServo(Servo servo, bool clockwise, int motorStep = 1) {
   }
 }
 
-
 /**
-   FOR DEBUGGING PURPOSE ONLY, DELETE THIS BEFORE SUBMITTING CODE
-*/
-
-void testLoop(){
-  //screen.clear();
-  //screen.print("TURNING LEFT");
-  turnServo(yaw,true, 30);
-  delay(1000);
-
-  //screen.clear();
-  //screen.print("TURNING RIGHT");
-  turnServo(yaw,false, 30);
-  delay(1000);
-
-  //screen.clear();
-  //screen.print("TURNING UP");
-  turnServo(pitch,true, 30);
-  delay(1000);
-
-  //screen.clear();
-  //screen.print("TURNING DOWN");
-  turnServo(pitch,false, 30);
-  delay(1000);
-}
-
+ * Clear the keypad buffer and reset it to the start
+ */
 void resetKeycode() {
   keyBufferHead = 0;
   for (int i = 0; i < KEY_BUFFER_SIZE; i++) {
@@ -81,15 +62,23 @@ void resetKeycode() {
   }
 }
 
+/**
+ * Handle a keypress event from the keypad to disable the alarm
+ */
 void keypadEvent(KeypadEvent key) {
   if (key == '*' && keyBufferHead > 0) {
     keyBuffer[--keyBufferHead] = NULL;
   } else if (key == '#' || keyBufferHead == KEY_BUFFER_SIZE) {
     screen.clear();
     if (String(keyBuffer).equals("291")) {
-      screen.print("Correct");
+      screen.print("Alarm Disable");
+      alarmIsOn = false;
+      digitalWrite(ENEMY_LED, LOW);
+      screen.setCursor(0, 1);
+      screen.clear();
+      screen.setCursor(0, 0);
     }else{
-      screen.print("Incorrect");
+      screen.print("Incorrect Code");
     }
     delay(2000);
     resetKeycode();
@@ -102,15 +91,18 @@ void keypadEvent(KeypadEvent key) {
    Set-ups required before the main loop
 */
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   screen.begin(16, 2);
   
-  yaw.attach(9);
-  pitch.attach(10);
+  pinMode(PIEZO_PIN, OUTPUT);
+  pinMode(ENEMY_LED, OUTPUT);
+  
+  yaw.attach(10);
+  pitch.attach(9);
   
   yaw.write(CENTER_MOST);
   pitch.write(CENTER_MOST);
-
+ 
   resetKeycode();
 }
 
@@ -125,16 +117,26 @@ void loop() {
       case 'r': turnServo(yaw, true, 2); break;
       case 'u': turnServo(pitch, true, 1); break;
       case 'd': turnServo(pitch, false, 1); break;
+      case 'a': alarmIsOn = true; break;
     }
   } 
   char key = keypad.getKey();
-  if (key != NULL) {
+  if (key != NULL && alarmIsOn) {
     keypadEvent(key);
     screen.clear();
+    screen.print("Enter Code: ");
+    screen.setCursor(0, 1);
     screen.print(String(keyBuffer));
-    //delay(100);
+    screen.setCursor(0, 0);
   }
 
+  if(alarmIsOn){
+    tone(PIEZO_PIN, 440, 50);
+    digitalWrite(ENEMY_LED, HIGH);
+    delay(50);
+    noTone(PIEZO_PIN);
+    delay(50);
+  }
 
 }
 
